c727ed4cfc9ac09af2c239b24136fd9f004c9e6e
obaro89 <igbinobaroosaretin@gmail.com>
2022-03-15 15:24:08 +0000

    updated package

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const { Command } = require("commander");
 const program = new Command();
 const FriendlyClock = require("./friendlyClock");
@@ -7,14 +8,10 @@ const friendlyClock = new FriendlyClock();
 program.version("1.0").description("Human friendly time to text");
 
 program
-  .option("-t, --inputtime [inputtime]", "Enter time input")
+  .command("showtime")
+  .alias("t")
   .description("Get the current human friendly time in text")
+  .action(() => {
+    console.log(friendlyClock.readTimeToText());
+  })
   .parse(process.argv);
-
-const options = program.opts();
-
-if (!options.inputtime) {
-  console.info("The user did not specify so get the default system time");
-} else {
-  console.log(options.inputtime);
-}
